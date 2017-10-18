3a5292199a195e0c4bf1ea9c9368ccbc8304cf77
Emory Merryman <emory.merryman@gmail.com>
2017-10-18 12:07:00 +0000

    fix command do not use while...use for instead

--- image/root/bin/archive-volume-ls.sh
+++ image/root/bin/archive-volume-ls.sh
@@ -10,9 +10,9 @@ do
         --mount type=volume,source=${VOLUME},destination=/volume,readonly=true \
         --workdir /volume \
         alpine:3.4 \
-            find \
+            for FILE in $(find \
             . \
-            -mindepth 1 | while read FILE
+            -mindepth 1)
             do
                 docker \
                     container \
@@ -26,30 +26,6 @@ do
                         -c %X \
                         "${FILE}"
             done | sort -n -r | head -n 1) &&
-        LAST_WRITE=$(docker \
-            container \
-            run \
-            --interactive \
-            --rm \
-            --mount type=volume,source=${VOLUME},destination=/volume,readonly=true \
-            --workdir /volume \
-            alpine:3.4 \
-                find \
-                . \
-                -mindepth 1 | while read FILE
-                do
-                    docker \
-                        container \
-                        run \
-                        --interactive \
-                        --rm \
-                        --mount type=volume,source=${VOLUME},destination=/volume,readonly=true \
-                        --workdir /volume \
-                        alpine:3.4 \
-                            stat \
-                            -c %Y \
-                            "${FILE}"
-                done | sort -n -r | head -n 1) &&
         SIZE=$(docker \
             container \
             run \
@@ -61,5 +37,5 @@ do
                 du \
                 -d1 \
                 /volume | cut -f1) &&
-        echo -e "${VOLUME}\t${LAST_READ}\t${LAST_WRITE}\t${SIZE}"
+        echo -e "${VOLUME}\t${LAST_READ}\t${SIZE}"
 done
