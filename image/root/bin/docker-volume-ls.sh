cffa75a069dd1382a57b43def2ec6795dc5818f8
Emory Merryman <emory.merryman@gmail.com>
2017-10-18 11:23:23 +0000

    fix ls

--- image/root/bin/docker-volume-ls.sh
+++ image/root/bin/docker-volume-ls.sh
@@ -2,7 +2,8 @@
 
 docker volume ls --quiet --filter label=name --filter label=description --filter label=expiry | while read VOLUME
 do
-    NAME="$(docker volume inspect --format \"{{.Labels.name}}\")" ${VOLUME} &&
-        DESCRIPTION="$(docker volume inspect --format \"{{.Labels.description}}\")" ${VOLUME} &&
-        EXPIRY="$(docker volume inspect --format \"{{.Labels.expiry}}\")" ${VOLUME}
+    NAME="$(docker volume inspect --format \"{{.Labels.name}}\" ${VOLUME})" &&
+        DESCRIPTION="$(docker volume inspect --format \"{{.Labels.description}}\" ${VOLUME})" &&
+        EXPIRY="$(docker volume inspect --format \"{{.Labels.expiry}}\" ${VOLUME})" &&
+        echo -en "${NAME}\t${DESCRIPTION}\t${EXPIRY}"
 done
