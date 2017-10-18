e00bd52f0db5a94a9a0417550b45644f02374817
Emory Merryman <emory.merryman@gmail.com>
2017-10-18 12:29:18 +0000

    do not use while

--- image/root/bin/archive-volume-ls.sh
+++ image/root/bin/archive-volume-ls.sh
@@ -2,7 +2,7 @@
 
 for VOLUME in $(docker volume ls --quiet)
 do
-    LAST_READ=$(docker \
+    FILES=$(docker \
         container \
         run \
         --interactive \
@@ -11,8 +11,9 @@ do
         --workdir /volume \
         alpine:3.4 \
             find \
-            . \
-            -mindepth 1 | while read FILE
+            .
+            -mindepth 1) &&
+        for FILE in ${FILES}
             do
                 docker \
                     container \
@@ -24,18 +25,6 @@ do
                     alpine:3.4 \
                         stat \
                         -c %X \
-                        "${FILE}"
-            done | sort -n -r | head -n 1) &&
-        # SIZE=$(docker \
-        #     container \
-        #     run \
-        #     --interactive \
-        #     --rm \
-        #     --mount type=volume,source=${VOLUME},destination=/volume,readonly=true \
-        #     --workdir /volume \
-        #     alpine:3.4 \
-        #         du \
-        #         -d1 \
-        #         /volume | cut -f1) &&
-        echo -e "${VOLUME}\t${LAST_READ}\t${SIZE}"
+                        ${FILE}
+            done | sort -n -r | head -n 1
 done
