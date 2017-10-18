98fbce578099e54d4c2d0e668e0fab34f4a080e1
Emory Merryman <emory.merryman@gmail.com>
2017-10-18 12:11:21 +0000

    only last read

--- image/root/bin/archive-volume-ls.sh
+++ image/root/bin/archive-volume-ls.sh
@@ -26,16 +26,16 @@ do
                         -c %X \
                         "${FILE}"
             done | sort -n -r | head -n 1) &&
-        SIZE=$(docker \
-            container \
-            run \
-            --interactive \
-            --rm \
-            --mount type=volume,source=${VOLUME},destination=/volume,readonly=true \
-            --workdir /volume \
-            alpine:3.4 \
-                du \
-                -d1 \
-                /volume | cut -f1) &&
+        # SIZE=$(docker \
+        #     container \
+        #     run \
+        #     --interactive \
+        #     --rm \
+        #     --mount type=volume,source=${VOLUME},destination=/volume,readonly=true \
+        #     --workdir /volume \
+        #     alpine:3.4 \
+        #         du \
+        #         -d1 \
+        #         /volume | cut -f1) &&
         echo -e "${VOLUME}\t${LAST_READ}\t${SIZE}"
 done
