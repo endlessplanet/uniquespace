7406f225e390984950598ecdf5dc4f27846544ff
Emory Merryman <emory.merryman@gmail.com>
2017-10-20 23:40:16 +0000

    wtf

--- image/root/bin/docker-volume-prune-empty.sh
+++ image/root/bin/docker-volume-prune-empty.sh
@@ -3,5 +3,5 @@
 docker volume ls --quiet | while read VOLUME
 do
     SIZE=$(docker container run --interactive --rm --mount type=volume,source=${VOLUME},destination=/volume,readonly=true alpine:3.4 du -d0 /volume) &&
-    echo -e "${VOLUME}\t${SIZE}"
+        echo -e "${VOLUME}\t${SIZE}"
 done
