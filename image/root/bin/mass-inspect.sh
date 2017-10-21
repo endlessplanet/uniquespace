2367a09e6a7fe4e7c65577d6558f4a67d076b0f2
Emory Merryman <emory.merryman@gmail.com>
2017-10-21 22:21:46 +0000

    wtf

--- image/root/bin/mass-inspect.sh
+++ image/root/bin/mass-inspect.sh
@@ -11,5 +11,5 @@ docker \
             echo "--mount type=volume,source=${VOLUME},destination=/srv/${VOLUME},readonly=true "
         done
     ) \
-    endlessplanet/volume-minder:a49d6c0cba53a09173af9eef4da12de94c383973 \
-    bash
+    alpine:3.4 \
+    find /srv -mindepth 1
