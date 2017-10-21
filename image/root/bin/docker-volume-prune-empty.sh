dddab121466823d26015c03ef6c17df209b89fda
Emory Merryman <emory.merryman@gmail.com>
2017-10-21 00:03:37 +0000

    find size

--- image/root/bin/docker-volume-prune-empty.sh
+++ image/root/bin/docker-volume-prune-empty.sh
@@ -2,6 +2,6 @@
 
 for VOLUME in $(docker volume ls --quiet)
 do
-    SIZE=$(docker container run --interactive --rm --mount type=volume,source=${VOLUME},destination=/volume,readonly=true alpine:3.4 du -d0 /volume | cut -f 1 -d " ") &&
+    SIZE=$(docker container run --interactive --rm --mount type=volume,source=${VOLUME},destination=/volume,readonly=true alpine:3.4 du -d0 /volume | cut -f1) &&
         echo -e "${VOLUME}\t${SIZE}"
-done
+done | sort -n -k2 > /tmp/volumes.txt
