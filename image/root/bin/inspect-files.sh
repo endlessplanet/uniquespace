dc00f035b4ab7b508d7d09a919a2f2ec1f777e22
Emory Merryman <emory.merryman@gmail.com>
2017-10-22 00:41:14 +0000

    fuckery

--- image/root/bin/inspect-files.sh
+++ image/root/bin/inspect-files.sh
@@ -2,5 +2,5 @@
 
 for FILE in $(find /srv -mindepth 1)
 do
-    stat -c "<file path=\"${FILE}\" lastAccessed=\"%X\" lastModified=\"%Y\" lastChanged=\"%Z\" size=\"last%s\"/>" ${FILE}
+    stat -c "<file volume=\"$(echo ${FILE} | cut -f2 -d "/")\" path=\"$(echo ${FILE} | cut -f2 -d "/")\" lastAccessed=\"%X\" lastModified=\"%Y\" lastChanged=\"%Z\" size=\"last%s\"/>" ${FILE}
 done
