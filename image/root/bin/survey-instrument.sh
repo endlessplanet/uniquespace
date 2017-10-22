8cc9edf32a875818e51734ed59dc9e65437bd50e
Emory Merryman <emory.merryman@gmail.com>
2017-10-22 18:04:31 +0000

    simplify we sort in standard order and take the last element

--- image/root/bin/survey-instrument.sh
+++ image/root/bin/survey-instrument.sh
@@ -4,18 +4,18 @@ echo -n "lastAccessed=\""
     for FILE in $(find /volume -mindepth 1)
     do
         stat -c %X ${FILE}
-    done | sort -nr | head -n 1 &&
+    done | sort -n | tail -n 1 &&
     echo -n "\" " &&
     echo -n "lastModified=\""
     for FILE in $(find /volume -mindepth 1)
     do
         stat -c %Y ${FILE}
-    done | sort -nr | head -n 1 &&
+    done | sort -n | tail -n 1 &&
     echo -n "\" " &&
     echo -n "lastChanged=\""
     for FILE in $(find /volume -mindepth 1)
     do
         stat -c %Z ${FILE}
-    done | sort -nr | head -n 1 &&
+    done | sort -n | tail -n 1 &&
     echo -n "\" " &&
     echo ">"
