39f4e799493e8d9fc61bdda218dc3ae2a5265d3d
Emory Merryman <emory.merryman@gmail.com>
2017-10-22 17:55:15 +0000

    fixed volume error

--- image/root/bin/survey-instrument.sh
+++ image/root/bin/survey-instrument.sh
@@ -3,6 +3,6 @@
 echo -n "lastAccessed=\""
     for FILE in $(find /volume -mindepth 1)
     do
-        stat -c %X /volume/${FILE}
+        stat -c %X ${FILE}
     done | sort -nr | head -n 1 &&
     echo -n "\">"
