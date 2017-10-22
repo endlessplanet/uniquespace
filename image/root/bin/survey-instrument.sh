f614c900665afc1e1c62c0f180ce1d645deedb4e
Emory Merryman <emory.merryman@gmail.com>
2017-10-22 18:24:09 +0000

    use quotes for files

--- image/root/bin/survey-instrument.sh
+++ image/root/bin/survey-instrument.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
-echo -n "lastAccessed=\""
+echo -n "lastAccessed=\"" &&
     for FILE in $(find /volume -mindepth 1)
     do
-        stat -c %X ${FILE}
+        stat -c %X "${FILE}"
     done | sort -n | tail -n 1 &&
     echo -n "\" " &&
-    echo -n "lastModified=\""
+    echo -n "lastModified=\"" &&
     for FILE in $(find /volume -mindepth 1)
     do
-        stat -c %Y ${FILE}
+        stat -c %Y "${FILE}"
     done | sort -n | tail -n 1 &&
     echo -n "\" " &&
-    echo -n "lastChanged=\""
+    echo -n "lastChanged=\"" &&
     for FILE in $(find /volume -mindepth 1)
     do
-        stat -c %Z ${FILE}
+        stat -c %Z "${FILE}"
     done | sort -n | tail -n 1 &&
     echo -n "\" " &&
+    echo -n "size=\"" &&
+    du -d0 /volume | cut -f1 &&
+    echo -n "\"" &&
     echo ">"
