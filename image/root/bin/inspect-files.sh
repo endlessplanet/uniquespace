ca087c82a3710de755e0cbf231b112a93bb7faf1
Emory Merryman <emory.merryman@gmail.com>
2017-10-22 17:14:51 +0000

    fuckery

--- image/root/bin/inspect-files.sh
+++ image/root/bin/inspect-files.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-for FILE in $(find /srv -mindepth 1)
+for FILE in $(find /srv -mindepth 2)
 do
     stat -c "<file volume=\"$(echo ${FILE} | cut -f3 -d "/")\" path=\"$(echo /$(echo ${FILE} | cut -f4 -d "/"))\" lastAccessed=\"%X\" lastModified=\"%Y\" lastChanged=\"%Z\" size=\"%s\"/>" ${FILE}
 done &&
-    for DU in $(du -d2 /srv)
+    for DU in $(du -d1 /srv)
     do
         VOLUME=$(echo ${DU} | cut -f2 | cut -f2 -d "/") &&
             SIZE=$(echo ${DU} | cut -f1) &&
