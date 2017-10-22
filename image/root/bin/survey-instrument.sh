69d41c2cc6d77c3e1ff0e19144073d54241e8830
Emory Merryman <emory.merryman@gmail.com>
2017-10-22 21:42:35 +0000

    fuckery

--- image/root/bin/survey-instrument.sh
+++ image/root/bin/survey-instrument.sh
@@ -12,12 +12,12 @@
 #         stat -c %Y "${FILE}"
 #     done | sort -n | tail -n 1 &&
 #     echo -n "\" " &&
-    echo -n "lastChanged=\"" &&
-    for FILE in $(find /volume -mindepth 1)
-    do
-        stat -c %Z "${FILE}"
-    done | sort -n | tail -n 1 &&
-    echo -n "\" " &&
+    # echo -n "lastChanged=\"" &&
+    # for FILE in $(find /volume -mindepth 1)
+    # do
+    #     stat -c %Z "${FILE}"
+    # done | sort -n | tail -n 1 &&
+    # echo -n "\" " &&
     echo -n "size=\"" &&
     du -d0 /volume | cut -f1 &&
     echo -n "\"" &&
