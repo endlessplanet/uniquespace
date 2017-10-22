7f05b1f4fdc67c3a51c518e33f0d9447a39fd7fd
Emory Merryman <emory.merryman@gmail.com>
2017-10-22 10:40:46 +0000

    do docker inspection

--- image/root/bin/inspect-files.sh
+++ image/root/bin/inspect-files.sh
@@ -2,5 +2,5 @@
 
 for FILE in $(find /srv -mindepth 1)
 do
-    stat -c "<file volume=\"$(echo ${FILE} | cut -f2 -d "/")\" path=\"$(echo ${FILE} | cut -f2 -d "/")\" lastAccessed=\"%X\" lastModified=\"%Y\" lastChanged=\"%Z\" size=\"last%s\"/>" ${FILE}
+    stat -c "<file volume=\"$(echo ${FILE} | cut -f3 -d "/")\" path=\"$(echo /$(echo ${FILE} | cut -f4 -d "/"))\" lastAccessed=\"%X\" lastModified=\"%Y\" lastChanged=\"%Z\" size=\"%s\"/>" ${FILE}
 done
--- image/root/bin/mass-inspect.sh
+++ image/root/bin/mass-inspect.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-cat /home/user/bin/inspect-files | docker \
-    container \
-    run \
-    --interactive \
-    --rm \
-    $(docker volume ls --quiet | head -n 100 | while read VOLUME
-        do
-            echo "--mount type=volume,source=${VOLUME},destination=/srv/${VOLUME},readonly=true "
-        done
-    ) \
-    alpine:3.4 \
-    sh
+echo "<survey>" &&
+    docker volume ls --quiet | head -n 10 | while read VOLUME
+    do
+        DRIVER=$(docker volume inspect ${VOLUME} --format "<inspection driver=\"{{ .Driver }}\" mountpoint=\"{{ .Mountpoint }}\" name={{ .Name }}\" scope=\"{{ .Scope }}\">") &&
+            echo "</inspection>"
+    done &&
+    cat /home/user/bin/inspect-files | docker \
+        container \
+        run \
+        --interactive \
+        --rm \
+        $(docker volume ls --quiet | head -n 10 | while read VOLUME
+            do
+                echo "--mount type=volume,source=${VOLUME},destination=/srv/${VOLUME},readonly=true "
+            done
+        ) \
+        alpine:3.4 \
+        sh &&
+    echo "</survey>"
