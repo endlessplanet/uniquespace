bf8da4afa58f0b3b5c32a16c52d8d1d3f38f53be
Emory Merryman <emory.merryman@gmail.com>
2017-10-22 23:43:06 +0000

    fix it

--- image/root/bin/oldies.sh
+++ image/root/bin/oldies.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xsltproc --stringparam expiry $(($(date +%s)-4*7*24*60*60)) candidates.xslt.xml survey.xml
+xsltproc --stringparam expiry $(($(date +%s)-4*7*24*60*60)) /opt/docker/etc/candidates.xslt.xml survey.xml
