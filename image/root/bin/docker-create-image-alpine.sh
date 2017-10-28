b4337d6a92b524242efc21bb59a896bef62fd493
Emory Merryman <emory.merryman@gmail.com>
2017-10-28 10:54:04 +0000

    wtf

--- image/root/bin/docker-create-image-alpine.sh
+++ image/root/bin/docker-create-image-alpine.sh
@@ -2,4 +2,4 @@
 
 cd $(mktemp -d) &&
     sed -e "s#\${PACKAGE_NAME}#${1}#" -e "s#\${ENTRYPOINT}#${2}#" -e "s#\${COMMAND}#${3}#" -e "wDockerfile" /opt/docker/etc/alpine.dockerfile.txt &&
-    docker image build .
+    docker image --quiet build .
