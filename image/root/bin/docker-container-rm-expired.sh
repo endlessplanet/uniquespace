529b605c5d9ddd5b0f85e2f1a28b6d1a08e29caf
Emory Merryman <emory.merryman@gmail.com>
2017-10-30 19:03:45 +0000

    fix container rm

--- image/root/bin/docker-container-rm-expired.sh
+++ image/root/bin/docker-container-rm-expired.sh
@@ -3,6 +3,6 @@
 docker container ls --all --quiet --filter label=expiry | while read CONTAINER
 do
     [ $(date +%s) -gt $(docker container inspect ${CONTAINER} --format "{{index .Config.Labels \"expiry\"}}") ] &&
-        [ "$(docker container inspect --format \"{{ .State.Status }}\")" != "running" ] &&
+        [ '$(docker container inspect --format "{{ .State.Status }}" ${CONTAINER})' != "running" ] &&
         docker container rm --volumes ${CONTAINER}
 done
