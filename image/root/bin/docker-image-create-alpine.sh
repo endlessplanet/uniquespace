ecde8913aa857d68b84f8847f348a5218c9adaf0
Emory Merryman <emory.merryman@gmail.com>
2017-10-30 11:48:48 +0000

    make docker dir

--- image/root/bin/docker-image-create-alpine.sh
+++ image/root/bin/docker-image-create-alpine.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd $(mktemp -d) &&
-    sed -e "s#\${PACKAGE_NAME}#${1}#" -e "s#\${ENTRYPOINT}#${2}#" -e "s#\${COMMAND}#${3}#" -e "wDockerfile" /opt/docker/etc/alpine.dockerfile.txt &&
+    sed -e "s#\${PACKAGE_NAME}#${1}#" -e "s#\${ENTRYPOINT}#${2}#" -e "s#\${COMMAND}#${3}#" -e "wDockerfile" /opt/docker/lib/docker/alpine/Dockerfile &&
     docker image --quiet build .
