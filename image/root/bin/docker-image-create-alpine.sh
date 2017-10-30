777bf11a3c6f028fb9098132441accd735cde9eb
Emory Merryman <emory.merryman@gmail.com>
2017-10-30 11:56:00 +0000

    reorg

--- image/root/bin/docker-image-create-alpine.sh
+++ image/root/bin/docker-image-create-alpine.sh
@@ -1,5 +1,32 @@
 #!/bin/sh
 
-cd $(mktemp -d) &&
-    sed -e "s#\${PACKAGE_NAME}#${1}#" -e "s#\${ENTRYPOINT}#${2}#" -e "s#\${COMMAND}#${3}#" -e "wDockerfile" /opt/docker/lib/docker/alpine/Dockerfile &&
-    docker image --quiet build .
+while [ ${#} -gt 0 ]
+do
+    case ${1} in
+        --maintainer)
+            MAINTAINER="${2}" &&
+                shift 2
+                ;;
+        --package-name)
+            PACKAGE_NAME="${2}" &&
+                shift 2
+                ;;
+        --entrypoint)
+            ENTRYPOINT="${2}" &&
+                shift 2
+                ;;
+        --commmand)
+            COMMAND="${2}" &&
+                shift 2
+                ;;
+    esac
+done &&
+    cd $(mktemp -d) &&
+        sed \
+            -e "s#\${MAINTAINER}#${MAINTAINER}#" \
+            -e "s#\${PACKAGE_NAME}#${PACKAGE_NAME}#" \
+            -e "s#\${ENTRYPOINT}#${ENTRYPOINT}#" \
+            -e "s#\${COMMAND}#${COMMAND}#" \
+            -e "wDockerfile" \
+            /opt/docker/lib/docker/alpine/Dockerfile &&
+        docker image --quiet build .
