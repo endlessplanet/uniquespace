634c872c6a7293d6a228bd190f52cb0883859796
Emory Merryman <emory.merryman@gmail.com>
2017-10-31 10:32:24 +0000

    fix array

--- bin/create.sh
+++ bin/create.sh
@@ -8,4 +8,5 @@ docker \
     --tty \
     --volume /var/run/docker.sock:/var/run/docker.sock:ro \
     --env DISPLAY \
+    --env MAINTAINER \
     endlessplanet/uniquespace:$(git rev-parse --verify HEAD)
