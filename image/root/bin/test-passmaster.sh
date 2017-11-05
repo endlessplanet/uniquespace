7151e607a3644d862f38259918e650e9a49e4bbb
Emory Merryman <emory.merryman@gmail.com>
2017-11-05 14:02:10 +0000

    rework

--- image/root/bin/test-passmaster.sh
+++ image/root/bin/test-passmaster.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-docker-image-build-fedora --title pass --package-name pass --entrypoint pass --command &&
-    docker-image-build-alpine --title gpgme --package-name gpgme --entrypoint gpg --command &&
-    docker-image-build-alpine --title findutils --package-name findutils --entrypoint find --command &&
-    docker-container-create --interactive --tty --title pass --image pass &&
-    docker-container-inject-run --title pass --image findutils --program-name find
+docker-image-build-alpine --title base --package-name bash --entrypoint bash --command &&
+    docker-image-build-fedora --title pass --package-name pass --entrypoint pass --command &&
+    docker-container-create --interactive --tty --title base --image base &&
+    docker-container-inject-run --title pass --image pass --program-name pass --container --interactive --tty --rm
