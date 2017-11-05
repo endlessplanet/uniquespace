25d1d66ef8a76462c0008090e9f77dc25f0d257b
Emory Merryman <emory.merryman@gmail.com>
2017-11-05 15:53:58 +0000

    fuckery

--- image/root/bin/test-passmaster.sh
+++ image/root/bin/test-passmaster.sh
@@ -3,4 +3,4 @@
 docker-image-build-alpine --title base --package-name bash --entrypoint bash --command &&
     docker-image-build-fedora --title pass --package-name pass --entrypoint pass --command &&
     docker-container-create --interactive --tty --title base --image base &&
-    docker-container-inject-run --title pass --image pass --program-name pass --container --interactive --tty --rm
+    docker-container-inject-run --title base --image pass --program-name pass --container --interactive --tty --rm
