0c8945828af26526bf3d3f5d3220587dd240a373
Emory Merryman <emory.merryman@gmail.com>
2017-11-18 03:52:09 +0000

    add more logging

--- image/root/src/bin/uniquespace-docker-volume-prune-expired.sh
+++ image/root/src/bin/uniquespace-docker-volume-prune-expired.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-docker volume ls --quiet --filter label=expiry | while read VOLUME
-do
-    [ "$(date +%s)" -gt "$(docker volume inspect --format "{{index .Labels \"expiry\"}}" ${VOLUME})" ] &&
-        docker volume rm ${VOLUME}
-done
+echo AA00110 &&
+    docker volume ls --quiet --filter label=expiry | while read VOLUME
+    do
+        [ "$(date +%s)" -gt "$(docker volume inspect --format "{{index .Labels \"expiry\"}}" ${VOLUME})" ] &&
+            docker volume rm ${VOLUME}
+    done
