1ed29c528565069199206d89c9e1b7673bbb8282
Emory Merryman <emory.merryman@gmail.com>
2017-11-18 03:33:02 +0000

    remove all from volume scripts

--- image/root/src/bin/uniquespace-docker-volume-prune-expired.sh
+++ image/root/src/bin/uniquespace-docker-volume-prune-expired.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker volume ls --quiet --all --filter label=expiry | while read VOLUME
+docker volume ls --quiet --filter label=expiry | while read VOLUME
 do
     [ "$(date +%s)" -gt "$(docker volume inspect --format "{{index .Labels \"expiry\"}}" ${VOLUME})" ] &&
         docker volume rm ${VOLUME}
