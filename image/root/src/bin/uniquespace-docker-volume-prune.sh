40f75e28fd7e116dc03a2481e69e4e8e3e14157b
Emory Merryman <emory.merryman@gmail.com>
2017-11-18 04:07:59 +0000

    use if to break infinite loop

--- image/root/src/bin/uniquespace-docker-volume-prune.sh
+++ image/root/src/bin/uniquespace-docker-volume-prune.sh
@@ -1,29 +1,24 @@
 #!/bin/sh
 
-fixit(){
-    echo FUCK IT &&
-        exit 65
-}
-    trap fixit EXIT &&
-    while [ ${#} -gt 0 ]
-    do
-        case ${1} in
-            deprecated)
-                uniquespace-docker-volume-prune-deprecated &&
-                    shift ${#}
-            ;;
-            expired)
-                echo AA00100 ${@} &&
-                    uniquespace-docker-volume-prune-expired &&
-                    echo AA00200 ${@} &&
-                    shift ${#} &&
-                    echo AA00300 ${@}
-            ;;
-            *)
-                echo Unknown Option &&
-                    echo ${0} &&
-                    echo "${@}" &&
-                    exit 64
-            ;;
-        esac
-    done
+while [ ${#} -gt 0 ]
+do
+    case ${1} in
+        deprecated)
+            uniquespace-docker-volume-prune-deprecated &&
+                shift ${#}
+        ;;
+        expired)
+            echo AA00100 ${@} &&
+                uniquespace-docker-volume-prune-expired &&
+                echo AA00200 ${@} &&
+                shift ${#} &&
+                echo AA00300 ${@}
+        ;;
+        *)
+            echo Unknown Option &&
+                echo ${0} &&
+                echo "${@}" &&
+                exit 64
+        ;;
+    esac
+done
