ff60483fc70142fa84195d10ad67ce6946eb6880
Emory Merryman <emory.merryman@gmail.com>
2017-11-18 04:25:26 +0000

    do not use quotes on expiry

--- image/root/src/bin/uniquespace-docker-container-prune-expired.sh
+++ image/root/src/bin/uniquespace-docker-container-prune-expired.sh
@@ -2,7 +2,7 @@
 
 docker container ls --quiet --all --filter label=commit-id | while read CONTAINER
 do
-    if [ "$(date +%s)" -gt "$(docker container inspect --format "{{index .Config.Labels \"expiry\"}}" ${CONTAINER})" ]
+    if [ $(date +%s) -gt $(docker container inspect --format "{{index .Config.Labels \"expiry\"}}" ${CONTAINER}) ]
     then
         docker container stop ${CONTAINER} &&
         docker container rm --volumes ${CONTAINER}
--- image/root/src/bin/uniquespace-docker-volume-prune-expired.sh
+++ image/root/src/bin/uniquespace-docker-volume-prune-expired.sh
@@ -2,7 +2,7 @@
 
 docker volume ls --quiet --filter label=expiry | while read VOLUME
     do
-        if [ "$(date +%s)" -gt "$(docker volume inspect --format "{{index .Labels \"expiry\"}}" ${VOLUME})" ]
+        if [ $(date +%s) -gt $(docker volume inspect --format "{{index .Labels \"expiry\"}}" ${VOLUME}) ]
         then
             docker volume rm ${VOLUME}
         fi
