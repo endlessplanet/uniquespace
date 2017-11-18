1b9351b5382f2ab8f7d501efae2e424bfa505e55
Emory Merryman <emory.merryman@gmail.com>
2017-11-18 05:02:53 +0000

    perpetual volumes are not mounted in any containers

--- image/root/src/bin/uniquespace-docker-volume-ls-perpetual.sh
+++ image/root/src/bin/uniquespace-docker-volume-ls-perpetual.sh
@@ -3,7 +3,8 @@
 docker volume ls --quiet | while read VOLUME
 do
     EXPIRATION_DATE=$(docker volume inspect --format "{{index .Labels \"expiry\"}}" ${VOLUME}) &&
-        if [ -z "${EXPIRATION_DATE}" ]
+        CONTAINERS=$(docker container ls --quiet --all --filter volume=${VOLUME}) &&
+        if [ -z "${EXPIRATION_DATE}" ] && [ -z "${CONTAINERS}" ]
         then
             echo ${VOLUME}
         fi
