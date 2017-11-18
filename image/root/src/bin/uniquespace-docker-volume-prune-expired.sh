b508d6d4b311bb4b7c72f9fe183a7f5369e4e2c9
Emory Merryman <emory.merryman@gmail.com>
2017-11-18 04:00:27 +0000

    finding inf loop

--- image/root/src/bin/uniquespace-docker-volume-prune-expired.sh
+++ image/root/src/bin/uniquespace-docker-volume-prune-expired.sh
@@ -6,7 +6,7 @@ echo AA00110 &&
         echo AA00111 &&
             [ "$(date +%s)" -gt "$(docker volume inspect --format "{{index .Labels \"expiry\"}}" ${VOLUME})" ] &&
             echo AA00112 &&
-            docker volume rm ${VOLUME}
+            docker volume rm ${VOLUME} &&
             echo AA00113
     done &&
     echo AA00120
