afa0415e4f33da1766d3fb7f87e1e65df05aca95
Emory Merryman <emory.merryman@gmail.com>
2017-11-18 04:06:40 +0000

    use if to break infinite loop

--- image/root/src/bin/uniquespace-docker-volume-prune-expired.sh
+++ image/root/src/bin/uniquespace-docker-volume-prune-expired.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-echo AA00110 &&
-    docker volume ls --quiet --filter label=expiry | while read VOLUME
+docker volume ls --quiet --filter label=expiry | while read VOLUME
     do
-        echo AA00111 &&
-            [ "$(date +%s)" -gt "$(docker volume inspect --format "{{index .Labels \"expiry\"}}" ${VOLUME})" ] &&
-            echo AA00112 &&
-            docker volume rm ${VOLUME} &&
-            echo AA00113
-    done &&
-    echo AA00120
+        if [ "$(date +%s)" -gt "$(docker volume inspect --format "{{index .Labels \"expiry\"}}" ${VOLUME})" ]
+        then
+            docker volume rm ${VOLUME}
+        fi
+    done
